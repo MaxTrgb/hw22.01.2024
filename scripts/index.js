6b0ad376ae3feb9590386320997f07ceaf4387f9
MaxTrgb <126002400+MaxTrgb@users.noreply.github.com>
2024-01-22 16:31:45 +0200

    just started

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+// Task 1
+//var userName = prompt("enter yor name:");
+//alert("hello, " + userName);
+
+/*Запросіть у користувача його рік народження, підрахуйте 
+скільки йому років і виведіть результат. Поточний рік 
+вкажіть у коді як константу. */
+
+// Task2 
+
+var userBirthYear = prompt("Enter your birth year:");
+const currentYear = 2024;
+alert("Your ")
